9cb9a5461b2f880ca42cb94d796652b0f1139b63
masupa <dmasupa4@gmail.com>
2024-08-12 19:31:41 +0200

    fastapi: raise HTTPExceptions and add status codes

--- FastAPI/books_project/books2.py
+++ FastAPI/books_project/books2.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from typing import Optional
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -47,22 +48,23 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     """Read and return list of
     all books"""
     return BOOKS
 
 
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book(book_id: int = Path(gt=0)):  # Add validation for Path Param
     """Read book given the book id"""
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.get("/books/")
+@app.get("/books/", status_code=status.HTTP_200_OK)
 # Add validation for Query Param
 async def read_books_by_rating(book_rating: int = Query(gt=0, lt=6)):
     """Read books with the passed rating"""
@@ -73,7 +75,7 @@ async def read_books_by_rating(book_rating: int = Query(gt=0, lt=6)):
     return books_to_return
 
 
-@app.get("/books/publish/")
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
 # Add validation for Query Param
 async def read_books_by_publish_date(published_date: int = Query(ge=1999, le=datetime.now().year)):
     """Read books with the published date"""
@@ -84,7 +86,7 @@ async def read_books_by_publish_date(published_date: int = Query(ge=1999, le=dat
     return books_to_return
 
 
-@app.post("/create-book")
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(body_request: BodyRequest):
     """ Adds a new book to the catalogue of books """
     new_book = Book(**body_request.model_dump())
@@ -102,20 +104,28 @@ def find_book_id(book: Book):
     return book
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_request: BodyRequest):
     """Update an existing book given the book
     title"""
+    book_changed = False
     for index, book in enumerate(BOOKS):
         if book.title.casefold() == book_request.title.casefold():
             BOOKS[index] = Book(**book_request.model_dump())
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 # Add validation for Path Parameter
 async def delete_book(book_id: int = Path(gt=0)):
     """Delete book from catalogue given it's book ID"""
+    book_deleted = False
     for index, book in enumerate(BOOKS):
         if book.id == book_id:
             BOOKS.pop(index)
+            book_deleted = True
             break
+    if not book_deleted:
+        raise HTTPException(status_code=404, detail='Item not found')
